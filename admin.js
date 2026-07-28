d73be48a2e604f9e2593307be362331850c46c12
GuptaCosmeticShop0Messhoseller <anshraj33668@gmail.com>
2026-07-28 18:11:37 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,37 @@
+const ADMIN_PASSWORD = "Ansh@2006";
 
+function login() {
+
+  const password = document.getElementById("password").value.trim();
+  const msg = document.getElementById("msg");
+
+  if (password === ADMIN_PASSWORD) {
+
+    localStorage.setItem("adminLoggedIn", "true");
+    window.location.href = "dashboard.html";
+
+  } else {
+
+    msg.innerHTML = "❌ Wrong Password";
+    msg.style.color = "red";
+
+  }
+
+}
+
+if (window.location.pathname.includes("dashboard.html")) {
+
+  if (localStorage.getItem("adminLoggedIn") !== "true") {
+
+    window.location.href = "admin.html";
+
+  }
+
+}
+
+function logout() {
+
+  localStorage.removeItem("adminLoggedIn");
+  window.location.href = "admin.html";
+
+}
